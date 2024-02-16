99a87bcf1411e4a301cd14e1a81e023d76cf3895
Thomas Lando <tlan3750@uni.sydney.edu.au>
2024-02-16 12:33:51 +1030

    fuck this

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from "./header/header.component";
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeaderComponent]
 })
 export class AppComponent {
-  title = 'Space And Rocketry Calculator (SpARC)';
 
   
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { MatTabGroup } from '@angular/material/tabs';
+import { MatTab } from '@angular/material/tabs';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatTabGroup, MatTab],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class HeaderComponent {
+  title: string = "Space and Rocketry Calculator (SpARC)";
 
+  customLabelClass = 'mat-tab-label-custom';
 }
